3f4053f884cef8e3e175173d1d59d3d26ce84f21
ThiagoRios04 <153288407+ThiagoRios04@users.noreply.github.com>
2024-01-22 10:38:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newTravel = "2024-2-16";
+
+function countdown() {
+
+    const newTravelDate = new Date(newTravel);
+    const currentDate = new Date();
+
+    const totalSeconds = (newTravelDate - currentDate) / 1000;
+    console.log(totalSeconds);
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hours.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+    
+}
+
+countdown();
+
+setInterval(countdown, 1000);
